6aeeba14c9d948990808ea89d9eb03f922d1ee68
Michael Fotiadis <mikemike19991@gmail.com>
2024-01-18 22:19:29 +0200

    Renamed to authResponse.

---
+++ src/app/models/authResponse.ts
@@ -0,0 +1,7 @@
+export class AuthResponse {
+  access_token!: string;
+  refresh_token!: string;
+  expires_in!: string;
+  refresh_expires_in!: string;
+  token_type!: string;
+}
--- src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    
-    id?:string
-    username: string = ""
-    password?: string
-    ots_Adeies_Employee_Id: string = ""
-    ots_Adeies_User_Is_Enabled?: Number
-      
-}
