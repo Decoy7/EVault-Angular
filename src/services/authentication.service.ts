aad614087d54e390199adb6068233e88f0c500ed
Michael Fotiadis <mikemike19991@gmail.com>
2024-01-18 22:17:53 +0200

    Renamed service to authentication and implemented two keycloak methods of login and logout.

---
+++ src/services/authentication.service.ts
@@ -0,0 +1,46 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  authURL: string = "http://localhost:8080/realms/Open5HR/protocol/openid-connect/token";
+  logoutURL: string = "http://localhost:8080/realms/Open5HR/protocol/openid-connect/logout";
+  constructor(private http: HttpClient) {}
+
+   login(username:string, password:string):Observable<User>{
+     const body:URLSearchParams = new URLSearchParams();
+     body.set("grant_type","password");
+     body.set("client_id","frontend");
+     body.set('username', username);
+     body.set('password', password);
+
+     const options = {
+       headers: new HttpHeaders()
+         .set('Content-Type', 'application/x-www-form-urlencoded'),
+         responseType: 'json' as 'json'
+     };
+    return (this.http.post<User>(this.authURL, body.toString(), options));
+  }
+
+  logout(){
+    const body:URLSearchParams = new URLSearchParams();
+    const refresh_token = localStorage.getItem("refresh_token")!;
+
+    body.set("client_id","frontend");
+    body.set("refresh_token",refresh_token);
+
+    let options = {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/x-www-form-urlencoded'),
+      responseType: 'json' as 'json'
+    };
+
+    localStorage.clear();
+    return this.http.post(this.logoutURL, body.toString(), options);
+  }
+
+}
