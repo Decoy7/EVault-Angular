a333d818d76cc71b2213986d2fa7b7497b3a881f
Michael Fotiadis <mikemike19991@gmail.com>
2024-01-18 22:28:13 +0200

    Basic logout component.

---
+++ src/app/logout-button/logout-button.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-logout-button',
+  templateUrl: './logout-button.component.html',
+  styleUrls: ['./logout-button.component.scss']
+})
+export class LogoutButtonComponent {
+  constructor(private authService:AuthenticationService, private router:Router) {}
+
+  Logout():void{
+    this.authService.logout()
+      .subscribe({
+        next: value => {
+          // console.log(value);
+        },
+        error:err => {
+          // console.log(err)
+          // Needs a toast here for error logging out
+        }
+      })
+    this.router?.navigateByUrl('login');
+  }
+
+}
