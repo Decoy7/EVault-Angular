0eb24234763af223be4ef8ffd15b8c7bb4a6e84f
Michael Fotiadis <mikemike19991@gmail.com>
2024-01-29 23:53:44 +0200

    Added conditional error toasts based on response status and success toasts.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,21 +29,36 @@ export class LoginComponent {
     const username = this.loginCredentials.get('username')!.value;
     const password = this.loginCredentials.get('password')!.value;
     this.authService.login(username, password).subscribe({
-      next: (data:AuthResponse):void=>{
-        localStorage.setItem('access_token', data.access_token);
-        localStorage.setItem('refresh_token', data.refresh_token);
-        localStorage.setItem('expires_in', data.expires_in);
-        localStorage.setItem('refresh_expires_in', data.refresh_expires_in);
-        localStorage.setItem('token_type', data.token_type);
+      next: (authResponse:AuthResponse):void=>{
+        localStorage.setItem('access_token', authResponse.access_token);
+        localStorage.setItem('refresh_token', authResponse.refresh_token);
+        localStorage.setItem('expires_in', authResponse.expires_in);
+        localStorage.setItem('refresh_expires_in', authResponse.refresh_expires_in);
+        localStorage.setItem('token_type', authResponse.token_type);
 
-        this.router?.navigateByUrl('/home');
-      },
-      error: ():void=>{
-        this.toast.error('Invalid credentials', 'Error', {
+        this.toast.success('Successfully logged in!', 'Success!', {
           timeOut: 3000,
           progressBar: true,
           positionClass: 'toast-top-right',
         });
+
+        this.router?.navigateByUrl('/home');
+      },
+      error:err => {
+        console.log(err);
+        if(err.status == 401){
+          this.toast.error('Invalid credentials', 'Error', {
+            timeOut: 3000,
+            progressBar: true,
+            positionClass: 'toast-top-right',
+          });
+        }else{
+          this.toast.error('Unexpected error!', 'Error', {
+            timeOut: 3000,
+            progressBar: true,
+            positionClass: 'toast-top-right',
+          });
+        }
       }
     });
   }
