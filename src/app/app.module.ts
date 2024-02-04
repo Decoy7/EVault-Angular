f0aade88eeef539bf540bd38b6c289f4c340ec81
Michael Fotiadis <mikemike19991@gmail.com>
2024-02-04 22:20:21 +0200

    Optimized imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { LandingComponent } from './landing/landing.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {LandingComponent} from './landing/landing.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card'
 import {MatFormFieldModule} from '@angular/material/form-field'
-import {FormsModule} from '@angular/forms';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
-import { LogoutButtonComponent } from './logout-button/logout-button.component';
+import {LogoutButtonComponent} from './logout-button/logout-button.component';
 import {ToastrModule} from "ngx-toastr";
-import { NavigationComponent } from './navigation/navigation.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import {NavigationComponent} from './navigation/navigation.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from "@angular/material/grid-list";
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatMenuModule } from '@angular/material/menu';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {MatMenuModule} from '@angular/material/menu';
 import {MatRippleModule} from "@angular/material/core";
 
 @NgModule({
