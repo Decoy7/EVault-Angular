ac91fa350d73f64f0e6fd771a07c116d3f36b85a
Michael Fotiadis <mikemike19991@gmail.com>
2024-02-04 14:07:02 +0200

    Added light mode dark mode option next to logout button along with toasts on change. TODO implement theme changing.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-navigation',
@@ -10,10 +11,28 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class NavigationComponent {
   private breakpointObserver = inject(BreakpointObserver);
+  isLightMode: boolean = false;
+
+  constructor(private toast:ToastrService) {}
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+
+  changeTheme() {
+    if(this.isLightMode){
+      this.toast.success('🔆 Changed to light mode! 🔆','',{
+        timeOut: 3000,
+        positionClass: 'toast-top-center',
+      });
+    }else{
+      this.toast.success('🌑 Changed to dark mode! 🌑', '', {
+        timeOut: 3000,
+        positionClass: 'toast-top-center',
+      });
+    }
+    this.isLightMode = !this.isLightMode;
+  }
 }
